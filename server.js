20ded1502882a65097c828ce3726b036078510f7
Evan Turner <evturn@gmail.com>
2015-04-26 02:25:01 -0400

    logger logging

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request	= require('request');
 var logger = require('morgan');
+var MarkovChain = require('markovchain').MarkovChain
+  , quotes = new MarkovChain({ files: 'quotes.txt' })
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'html');
+app.use(logger('dev'));
 app.use(express.static(__dirname + '/assets'))
 
 app.get('/', function(req, res) {
