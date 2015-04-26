38b90b3289807cae5220a83c58652f9fff227bf1
Evan Turner <evturn@gmail.com>
2015-04-26 04:08:40 -0400

    function to render one blog post written in view to contain all posts

--- assets/js/views/blog-posts.js
+++ assets/js/views/blog-posts.js
@@ -1,5 +1,15 @@
 var app = app || {};
 
 app.BlogPosts = Backbone.View.extend({
-	
+	el: '.blog-posts',
+	initialize: function() {
+		this.addAll();
+	},
+	addOne: function(model) {
+		var view = new app.BlogPost(model: model);
+		this.$el.append(view.el);
+	},
+	addAll: function() {
+		
+	},
 });
