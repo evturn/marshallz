2ae65809c43832355f36da9bc945b44305afc55f
Evan Turner <evturn@gmail.com>
2015-04-26 02:32:48 -0400

    phrases being taken from quotes.txt

--- server.js
+++ server.js
@@ -11,10 +11,30 @@ app.set('view engine', 'html');
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/assets'))
 
+
 app.get('/', function(req, res) {
 	res.render('index');
 });
 
+app.get('/api/quotes', function(req, res) {
+	res.json(phrase.sentence);
+})
+
+var useUpperCase = function(wordList) {
+  var tmpList = Object.keys(wordList).filter(function(word) {
+  	return word[0] >= 'A' && word[0] <= 'Z' 
+  })
+  return tmpList[~~(Math.random()*tmpList.length)]
+}
+
+var phrase = quotes
+  .start(useUpperCase) // 
+  .end()
+  .process(function(err, s) {
+    console.log(s)
+    return s;
+  })
+
 app.listen(app.get('port'), function() {
-	console.log('Express listening somewhere. Come on, you know where.')
+	console.log('Express listening on 3000')
 });
