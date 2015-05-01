d742a45731681814c16fdba7298aed7be60ec71b
Evan Turner <evturn@gmail.com>
2015-05-01 14:57:27 -0400

    promise fixed on client

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,15 @@ var app = app || {};
 var ref = new Firebase('https://marshallz.firebaseio.com/posts');
 
 app.posts = new app.Posts();
-app.posts.fetch();
-var blogPosts = new app.BlogPosts({collection: app.posts});
+
+var promise = new Promise(function(resolve, reject) {
+	var collection = app.posts.fetch({
+		success: function(data) {
+			console.log('we got: ', data);
+			resolve(collection);
+		}
+	});
+});
+promise.then(function() {
+	var blogPosts = new app.BlogPosts({collection: app.posts});
+});
