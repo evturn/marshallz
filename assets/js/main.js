ecdc78574e61ad43e6281a3f59519e2fc0d1e58d
Evan Turner <evturn@gmail.com>
2015-05-01 13:22:17 -0400

    more quotes added

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,5 @@ var app = app || {};
 var ref = new Firebase('https://marshallz.firebaseio.com/posts');
 
 app.posts = new app.Posts();
-
-var promise = new Promise(function(resolve, reject) {
-	var collection = app.posts.fetch({
-		success: function(data) {
-			console.log('we got: ', data);
-			resolve(collection);
-		}
-	});
-});
-promise.then(function() {
-	var blogPosts = new app.BlogPosts({collection: app.posts});
-});
+app.posts.fetch();
+var blogPosts = new app.BlogPosts({collection: app.posts});
