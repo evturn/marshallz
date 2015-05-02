ecb26f0ce70ee5bab8cdea40e21a759ede923954
Evan Turner <evturn@gmail.com>
2015-05-02 10:34:03 -0400

    tweets moved into own module

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-var express 		= require('express');
-var Firebase    = require("firebase");
-var logger 			= require('morgan');
-var MarkovChain = require('markovchain').MarkovChain
-  , title 			= new MarkovChain({files: 'quotes.txt'});
-var Twitter     = require('twitter');
-var Promise     = require('promise');
-var posts       = require('./lib/posts.js');
-var app 				= express();
+var express 	= require('express');
+var logger 		= require('morgan');
+var Twitter   = require('twitter');
+var Promise   = require('promise');
+var blog      = require('./lib/posts.js');
+var tweet     = require('./lib/tweets.js');
+var app 		  = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'html');
@@ -17,54 +15,15 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
-app.get('/api/quotes', function(req, res) {
+app.get('/api', function(req, res) {
   var post = newPost();
   res.status(200).json(post);
 });
 
 
-setInterval(posts, 3600000);  // 3600000: 6 hours
+setInterval(blog, 3600000);  // 3600000: 1 hour
 
-
-// twitter
-var client = new Twitter({
-  consumer_key: process.env.MARSHALLZ_TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.MARSHALLZ_TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.MARSHALLZ_TWITTER_TOKEN_KEY,
-  access_token_secret: process.env.MARSHALLZ_TWITTER_TOKEN_SECRET
-});
-
-
-setInterval(writeTweet, 21600000)
-
-function writeTweet() {
-  
-  var update = newPost();
-  var status = update.title;
-  console.log('Tweet String: ', status);
-  sendTweet(status);
-  // client.post('statuses/update', {status: update},  function(error, tweet, response){
-  //     if(error) { 
-  //       throw error 
-  //     } else {
-  //       console.log('Tweet: ', tweet);  // Tweet body. 
-  //       console.log(response);  // Raw response object. 
-  //     }
-  //     tweetArray.length = 0;
-  // });
-}
-var sendTweet = function(phrase) {
-client.post('statuses/update', {status: phrase},  function(error, tweet, response){
-      if(error) { 
-        throw error 
-      } else {
-        console.log('Tweet: ', tweet);  // Tweet body. 
-        console.log(response);  // Raw response object. 
-      }
-  });
-}
-
-
+setInterval(tweet, 20000000) // 21600000: 6 hours
 
 var port = app.get('port');
 app.listen(port, function() {
