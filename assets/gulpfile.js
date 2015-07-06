0a61f5cd9a6d9c8746e2d3e7882b32b5968420d6
Evan Turner <evturn@gmail.com>
2015-07-06 06:08:29 -0400

    updates made to paths

---
+++ assets/gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),  
+    G = require('gulp-load-plugins')();
+var paths = require('./config/paths');
+var options = require('./config/gulp-options');
