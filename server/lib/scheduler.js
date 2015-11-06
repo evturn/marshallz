8662930a3173d26cfed93f3cdc1a090e25b7b1eb
Evan Turner <evturn@gmail.com>
2015-11-05 21:27:35 -0500

    Add markdown for using crontabs

--- server/lib/scheduler.js
+++ server/lib/scheduler.js
@@ -2,25 +2,53 @@
 const Cron = require('cron').CronJob;
 const postToTwitter = require('./twitter-post');
 const postToBlog = require('./blog-post');
-const cron = {};
 
-cron.twitterFourAM = new Cron('00 00 04 * * *', function() {
+const jobs = [
+  {
+    media: 'twitter',
+    time: '4:00am',
+    repeats: 'daily',
+    crontab: '00 00 04 * * *'
+  }, {
+    media: 'twitter',
+    time: '10:00am',
+    repeats: 'daily',
+    crontab: '00 00 10 * * *'
+  }, {
+    media: 'twitter',
+    time: '2:00pm',
+    repeats: 'daily',
+    crontab: '00 00 16 * * *'
+  }, {
+    media: 'twitter',
+    time: '10:00pm',
+    repeats: 'daily',
+    crontab: '00 00 22 * * *'
+  }, {
+    media: 'blog',
+    time: '*:00am',
+    repeats: 'hourly',
+    crontab: '00 00 * * * *'
+  }
+];
+
+cron.twitterFourAM = new Cron('00 00 04 * * *', () => {
   postToTwitter();
 }, null, true);
 
-cron.twitterTenAM = new Cron('00 00 10 * * *', function() {
+cron.twitterTenAM = new Cron('00 00 10 * * *', () => {
   postToTwitter();
 }, null, true);
 
-cron.twitterTwoPM = new Cron('00 00 16 * * *', function() {
+cron.twitterTwoPM = new Cron('00 00 16 * * *', () => {
   postToTwitter();
 }, null, true);
 
-cron.twitterTenPM = new Cron('00 00 22 * * *', function() {
+cron.twitterTenPM = new Cron('00 00 22 * * *', () => {
   postToTwitter();
 }, null, true);
 
-cron.blogHourly = new Cron('00 00 * * * *', function() {
+cron.blogHourly = new Cron('00 00 * * * *', () => {
   postToBlog();
 }, null, true);
 
