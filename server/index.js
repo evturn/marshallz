ff892c53fc42f8b1de198ee5e90226813fc10244
Evan Turner <evturn@gmail.com>
2016-02-16 14:00:40 -0500

    Create function to ignore webpack app.server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 require('babel-core/register')({
-  only: /server/
+  only: /server/,
+  ignore: function(filename) {
+    const allowed = filename === '../public/assets/app.server.js' ? false : true;
+
+    return allowed;
+  }
 });
 require('./routes/config');
