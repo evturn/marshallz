2d69999ce243e66ffea972f395eaf218519bd6d8
Evan Turner <evturn@gmail.com>
2016-04-21 16:53:11 -0400

    Update pagination action

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -14,32 +14,31 @@ export const fetchPost = slug => dispatch => {
     .catch(err => dispatch(actions.fetchError(err)))
 }
 
+const perPage = 10;
+
 const setPagination = ({ posts, page }) => {
-  const perPage = 10;
-
-  const total = posts.length;
-  const pages = Math.ceil(total / perPage);
-
-  const previous = page > 1 ? page - 1 : false;
-  const next = page < pages ? page + 1 : false;
-  const first = ((page - 1) * perPage ) + 1;
-  const last = page * perPage;
-
-  const buttons = posts.map((item, i) => i + 1).filter(i => i <= pages);
-
-  const showing = posts.filter((post, i) => i >= first - 1 && i <= last - 1);
+  const first = ((page - 1) * perPage) + 1
+  const last = page * perPage
+  const pages = Math.ceil(posts.length / perPage)
 
   return {
-    showing,
+    showing: posts.filter((x, i) => i >= first - 1 && i <= last - 1),
     pagination: {
-      perPage, total, pages, page,
-      buttons, previous, next, first, last
+      first,
+      last,
+      pages,
+      page,
+      previous: page > 1 ? page - 1 : false,
+      next: page < pages ? page + 1 : false,
+      total: posts.length,
+      buttons: posts.map((x, i) => i + 1).filter(i => i <= pages)
     }
-  };
+  }
 }
 
 export const filterPosts = (
-  { params: { author }, query: { page }, filter }) => dispatch => {
+  { params: { author }, query: { page }, filter }
+) => dispatch => {
 
   dispatch(actions.filterPosts(setPagination({
     posts: author ? filter.author[author] : filter.all,
