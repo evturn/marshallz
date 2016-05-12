df964736b2986a424f5a04d61f0b84066428a06d
Evan Turner <evturn@gmail.com>
2016-05-11 23:28:28 -0400

    Create second sentence generator node readFule

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,4 @@
 global.__DEV__ = process.env.NODE_ENV === 'development'
 require('babel-register')({})
 require('./routes')
+require('./sandbox')
---
+++ src/server/sandbox/index.js
@@ -0,0 +1,83 @@
+import { Observable } from 'rx'
+import bots from '../bots'
+import fs from 'fs'
+import { rss as log } from '../../webpack/dev-logger'
+
+const norm = x => x !== undefined ? x.replace(/\.$/ig, '') : '';
+const done = x => x.lookup[x.selection] && !(x.state.split(' ').length > 14)
+
+function main() {
+  const dictionary$ = Observable.of(bots[0])
+    .map(x => fs.readFileSync(x.content).toString())
+    .flatMap(createDictionary)
+
+  const initialWord$ = dictionary$
+    .map(selectCapitalizedWord)
+
+  const sentence$ = Observable.combineLatest(dictionary$, initialWord$)
+    .map(([ dictionary, initialWord ]) => {
+      return {
+        lookup: dictionary,
+        selection: initialWord,
+        state: initialWord
+      }
+    })
+    .flatMap(x => Observable.generate(x, done, lookupAndConcat, x => x.state))
+    .debounce(1000)
+    .subscribe(log.observer)
+}
+
+function createDictionary(data) {
+  let acc = {}
+  data
+    .split(/(?:\. |\n)/ig)
+    .forEach(line => {
+      line
+        .split(' ')
+        .filter(word => word.trim() !== '')
+        .map((x, i, arr) => {
+          const curr = norm(arr[i])
+          const next = norm(arr[i + 1])
+          if (!acc[curr]) {
+            acc[curr] = {}
+          }
+          if (!acc[curr][next]) {
+            acc[curr][next] = 1
+          } else {
+            acc[curr][next] += 1
+          }
+        })
+    })
+  return Observable.of(acc)
+}
+
+function lookupAndConcat({ state, lookup, selection }) {
+  let nextState;
+  const term = lookup[selection]
+  const keys = Object.keys(term)
+  const sum = keys.reduce((acc, w) => acc + term[w], 0)
+  if (!Number.isFinite(sum)) {
+    throw new Error('All values in object must be a numeric value')
+  }
+  const predicator = ~~(Math.random() * sum)
+  keys.forEach(function(x, i) {
+    this.count += term[keys[i]]
+    if (this.count > predicator && !nextState) {
+      nextState = {
+        lookup: lookup,
+        selection: keys[i],
+        state: state + ' ' + keys[i]
+      }
+    }
+  }, { count: 0 })
+  return nextState
+}
+
+function selectCapitalizedWord(x) {
+  const caps = Object.keys(x)
+    .filter(([ x ]) => x >= 'A' && x <= 'Z')
+
+  return caps[~~(Math.random() * caps.length)]
+}
+
+export default main()
