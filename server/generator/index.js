cc24682704477abe2d8e373f9aaf0056804154d0
Evan Turner <evturn@gmail.com>
2016-08-27 20:10:43 -0400

    Begin rewrite of markov chain

--- server/generator/index.js
+++ server/generator/index.js
@@ -1,4 +1,5 @@
 import request from 'request'
+import { Author } from '../models'
 
 function readRemoteContent(author) {
   const req = request.get(author.content)
@@ -6,6 +7,63 @@ function readRemoteContent(author) {
   req.on('response', res => {
     let content = ''
     res.on('data', data => content += data.toString('utf8'))
-    req.on('end', _ => console.log(content))
+    req.on('end', _ => {
+      const tree = mapWordsByWeight(content)
+      console.log(tree)
+    })
   })
+}
+
+function mapWordsByWeight(str) {
+  return str
+    .split(/(?:\. |\n)/ig)
+    .map(sentence => {
+      return sentence
+        .split(' ')
+        .filter(word => word.trim() !== '')
+        .map(x => x.replace(/\.$/ig, ''))
+        .map((x, i, arr) => ({ curr: x, next: arr[i + 1] }))
+        .filter(x => x.next)
+        .reduce((acc, x) => {
+          if (!acc[x.curr]) {
+            acc[x.curr] = {}
+          }
+          if (!acc[x.curr][x.next]) {
+            acc[x.curr][x.next] = 1
+          } else {
+            acc[x.curr][x.next] += 1
+          }
+          return acc
+        }, {})
+    })
+}
+
+function getStartingPoint(tree) {
+  const caps = Object.keys(tree).filter(x => x[0] >= 'A' && x[0] <= 'Z')
+  return caps[~~(Math.random() * caps.length)]
+}
+
+function selectFollowingWord(obj) {
+  const keys = Object.keys(obj);
+  const sum = keys.reduce((p, c) => p + obj[c], 0);
+
+  if (!Number.isFinite(sum)) {
+    throw new Error('All values in object must be a numeric value');
+  }
+
+  const select = ~~(Math.random() * sum);
+
+  for (let i = 0, count = 0; i < keys.length; i++) {
+    count += obj[keys[i]];
+    if (count > select) {
+      return keys[i];
+    }
+  }
+}
+
+export default _ => {
+  Author
+    .findOne({ name: 'Marshall' })
+    .exec()
+    .then(readRemoteContent)
 }
