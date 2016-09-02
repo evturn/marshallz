67b06ee228f737205c38f1cd0eaceae929307f74
Evan Turner <evturn@gmail.com>
2016-09-01 23:42:38 -0400

    Update loading indicator

--- app/components/LoadingIndicator/index.js
+++ app/components/LoadingIndicator/index.js
@@ -4,18 +4,18 @@ import css from './styles.css'
 export default _ => (
   <div className={css.loading}>
     <div className={css['x-fading-circle']}>
-      <div className={css.xCircle}></div>
-      <div className={css['x-circle2']}></div>
-      <div className={css['x-circle3']}></div>
-      <div className={css['x-circle4']}></div>
-      <div className={css['x-circle5']}></div>
-      <div className={css['x-circle6']}></div>
-      <div className={css['x-circle7']}></div>
-      <div className={css['x-circle8']}></div>
-      <div className={css['x-circle9']}></div>
-      <div className={css['x-circle10']}></div>
-      <div className={css['x-circle11']}></div>
-      <div className={css['x-circle12']}></div>
+      <div className={css.xCircle} />
+      <div className={css['x-circle2']} />
+      <div className={css['x-circle3']} />
+      <div className={css['x-circle4']} />
+      <div className={css['x-circle5']} />
+      <div className={css['x-circle6']} />
+      <div className={css['x-circle7']} />
+      <div className={css['x-circle8']} />
+      <div className={css['x-circle9']} />
+      <div className={css['x-circle10']} />
+      <div className={css['x-circle11']} />
+      <div className={css['x-circle12']} />
     </div>
   </div>
 )
