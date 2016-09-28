d6e1a6d45866657712b91996c84a1bee3cdb85af
Evan Turner <evturn@gmail.com>
2016-09-28 15:02:54 -0400

    Break up global reducer

--- app/containers/Navigation/reducer.js
+++ app/containers/Navigation/reducer.js
@@ -1,46 +1,93 @@
-export default (state={
-  loading: false,
-  error: null,
-  posts: [],
-  authors: [],
-  author: {},
-  meta: {},
-  url: null,
-}, action) => {
+import { combineReducers } from 'redux'
+
+const loadingReducer = (state=false, action) => {
   switch (action.type) {
 
-    case 'FETCH_INITIAL_DATA':
-      return Object.assign({}, state, {
-        loading: true,
-      })
-
-    case 'INITIAL_FETCH_SUCCESS':
-      return Object.assign({}, state, {
-        ...action.payload,
-        loading: false,
-      })
-
     case 'FETCH':
-      return Object.assign({}, state, {
-        loading: true,
-      })
+      return true
 
     case 'FETCH_SUCCESS':
-      return Object.assign({}, state, {
-        ...action.payload,
-        loading: false,
-      })
+    case 'FETCH_ERROR':
+      return false
+
+    default:
+      return state
+  }
+}
+
+const errorReducer = (state=null, action) => {
+  switch (action.type) {
+
+    case 'FETCH':
+      return null
 
     case 'FETCH_ERROR':
-      return Object.assign({}, state, {
-        error: action.payload.error,
-        loading: false,
-      })
+      return action.payload.error.message
 
-    case 'NAVIGATE_TO_SINGLE_POST':
-      return Object.assign({}, state, {
-        meta: {},
-      })
+    default:
+      return state
+  }
+}
+
+const postsReducer = (state=[], action) => {
+  switch (action.type) {
+
+    case 'FETCH_SUCCESS':
+      return action.payload.posts
+
+    default:
+      return state
+  }
+}
+
+const authorsReducer = (state=[], action) => {
+  switch (action.type) {
+
+    case 'FETCH_SUCCESS':
+      return action.payload.authors
+
+    default:
+      return state
+  }
+}
+
+const authorReducer = (state={}, action) => {
+  switch (action.type) {
+
+    case 'FETCH_SUCCESS':
+      return action.payload.author || {}
+
+    default:
+      return state
+  }
+}
+
+const paginationReducer = (state={}, action) => {
+    switch (action.type) {
+
+    case 'FETCH_SUCCESS':
+      return action.payload.meta || {}
+
+    default:
+      return state
+  }
+}
+
+export default combineReducers({
+  posts: postsReducer,
+  authors: authorsReducer,
+  author: authorReducer,
+  meta: paginationReducer,
+  error: errorReducer,
+  loading: loadingReducer,
+})
+
+
+export const routeReducer = (state=null, action) => {
+  switch (action.type) {
+
+    case 'FETCH_SUCCESS':
+      return action.payload.url
 
     default:
       return state
--- app/reducers.js
+++ app/reducers.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux'
-import appReducer from './containers/Navigation/reducer'
+import appReducer, { routeReducer } from './containers/Navigation/reducer'
 import locationParamsReducer from './containers/ByAuthor/reducer'
 
 export default combineReducers({
   global: appReducer,
   routing: combineReducers({
     params: locationParamsReducer,
+    url: routeReducer,
   })
 })
