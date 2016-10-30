5bcfa390876599431a61cbfaabd3430ddedf0b3f
Evan Turner <evturn@gmail.com>
2016-10-30 16:42:03 -0400

    Create new flow to exec jobs

--- server/generator/index.js
+++ server/generator/index.js
@@ -1,36 +1,82 @@
-import fs from 'fs'
-import { CronJob } from 'cron'
-import { Author } from '../models'
-import blog from './blog'
-import twitter from './twitter'
+import axios from 'axios'
+import firebase from 'firebase'
+import createGenerator from './instance'
+import { CronJob as Cron } from 'cron'
+import { Observable as Ob$ } from 'rxjs'
 
-function startCronJobs() {
-  Author
-    .find()
-    .exec()
-    .then(xs => {
-      return xs
-        .map(x => x.toObject())
-        .map(createAuthorJobs)
-    })
-    .catch(e => console.log(e))
+const api = firebase.initializeApp({
+  apiKey: 'AIzaSyBqnVMFW5iidbPpvNL-Vmc5rVf4Cn1Uqq4',
+  authDomain: 'marshallz-5ff65.firebaseapp.com',
+  databaseURL: 'https://marshallz-5ff65.firebaseio.com/',
+  storageBucket: 'gs://marshallz-5ff65.appspot.com',
+})
+
+const subscriber = {
+  next: x => console.log(x),
+  error: e => console.error(`Jesus fuck! ${e}`),
+  complete: _ => console.log(`Bye.`)
 }
 
-function createAuthorJobs(author) {
-  author.cronjobs.map(x => {
-    return new CronJob(x.cron, dispatchJob(x.type, author), null, true)
-  })
+export default function main() {
+  return Ob$
+    .fromPromise(fetchInitialData())
+    .mergeMap(selectJobsToExec)
+    .mergeMap(populateGenerator)
+    .subscribe(subscriber)
 }
 
-function dispatchJob(jobType, author) {
-  const jobFn = jobType === 'twitter' ? twitter : blog
-  return _ => {
-    fs.readFile(`assets/${author.content}`, (e, data) => {
-      if (!e && data) {
-        jobFn(author, data.toString())
-      }
-    })
-  }
+function populateGenerator(x) {
+  return Ob$
+    .fromPromise(fetchAuthor(x.index))
+    .map(filterSourceURL)
+    .mergeMap(x => Ob$.fromPromise(fetchAuthorContent(x)))
+    .map(createGenerator)
+    .map(x.fn)
 }
 
-export default startCronJobs()
+function selectJobsToExec(data) {
+  return Ob$
+    .from(data.map(filterCronjobs))
+    .mergeMap(x => Ob$.from(x))
+}
+
+function filterCronjobs(data) {
+  const jobs = {blog: writeBlogPost, twitter: writeTwitterPost}
+  return data.cronjobs.map(x => ({fn: jobs[x.type], index: x.index}))
+}
+
+function filterSourceURL(data) {
+  return data.source_url
+}
+
+function writeBlogPost(gen) {
+  return gen.take(8)
+}
+
+function writeTwitterPost(gen) {
+  return gen.run()
+}
+
+function fetchInitialData() {
+  return api
+    .database()
+    .ref()
+    .child(`authors`)
+    .once('value')
+    .then(x => x.val())
+}
+
+function fetchAuthor(index) {
+  return api
+    .database()
+    .ref()
+    .child(`authors/${index}`)
+    .once('value')
+    .then(x => x.val())
+}
+
+function fetchAuthorContent(url) {
+  return axios
+    .get(process.env[url])
+    .then(x => x.data)
+}
--- server/index.js
+++ server/index.js
@@ -1,33 +1,35 @@
 import 'babel-polyfill'
 import express from 'express'
-import bodyParser from 'body-parser'
-import configStatic from './middleware/config-static'
-import authorData from './middleware/author-data'
-import postsByAuthor from './middleware/by-author'
-import postsByDate from './middleware/by-date'
-import postBySlug from './middleware/by-slug'
+// import bodyParser from 'body-parser'
+// import configStatic from './middleware/config-static'
+// import authorData from './middleware/author-data'
+// import postsByAuthor from './middleware/by-author'
+// import postsByDate from './middleware/by-date'
+// import postBySlug from './middleware/by-slug'
 import generator from './generator'
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
-const sendFile = configStatic(app)
+generator()
 
-app.get('/api?',
-  authorData,
-  postsByDate
-)
+// app.use(bodyParser.urlencoded({ extended: true }))
+// const sendFile = configStatic(app)
 
-app.get('/api/authors/:author?',
-  authorData,
-  postsByAuthor
-)
+// app.get('/api?',
+//   authorData,
+//   postsByDate
+// )
 
-app.get('/api/post/:slug',
-  authorData,
-  postBySlug
-)
+// app.get('/api/authors/:author?',
+//   authorData,
+//   postsByAuthor
+// )
 
-app.get('*', sendFile)
+// app.get('/api/post/:slug',
+//   authorData,
+//   postBySlug
+// )
+
+// app.get('*', sendFile)
 
 app.listen(3000, _ => console.log('Listening 🌐'))
