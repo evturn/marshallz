d26a86f8369d46bcf960d3a59128aa46491ec42d
Evan Turner <ev@evturn.com>
2017-12-29 12:53:14 -0500

    Run Haskell executable in a separate process

---
+++ exec/src/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import           Run
+
+str :: String
+str = "MARSHALLZ_SOURCE_URL_0"
+
+main :: IO ()
+main = do
+  run str
---
+++ exec/src/Run.hs
@@ -0,0 +1,22 @@
+#!/usr/bin/env stack
+-- stack --install-ghc runghc --package turtle
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Run ( run ) where
+
+import qualified Data.Text as T
+import           Turtle
+
+cmd :: T.Text
+cmd = "jaws fromURL "
+
+execJaws :: T.Text -> IO ()
+execJaws x = stdout (inshell (cmd <> x) empty)
+
+run :: String -> IO ()
+run str = do
+  x <- need (T.pack str)
+  case x of
+    Nothing -> return ()
+    Just x  -> execJaws x
